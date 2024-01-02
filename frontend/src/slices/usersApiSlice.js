1842d5328833f6fc03da1cb2a787b08a401c2f3e
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-01-02 11:05:50 -0600

    login functionality, user logout

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -6,13 +6,20 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 		login: builder.mutation({
 			query: (data) => ({
 				//replaces regular fetch request, useEffect and useState we had on HomeScreen
-				url: USERS_URL / auth,
-				method: POST,
+				url: `${USERS_URL}/auth`,
+				method: "POST",
 				body: data,
 			}),
 			keepUnusedDataFor: 5,
 		}),
+		//logout server
+		logout: builder.mutation({
+			query: () => ({
+				url: `${USERS_URL}/logout`,
+				method: "POST",
+			}),
+		}),
 	}),
 });
 
-export const { useLoginMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
