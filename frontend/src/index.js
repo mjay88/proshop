442169b3d7253e9da53edb4c2ac80d68e21b1448
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-01-04 14:01:01 -0600

    react-PayPal integration, paypal buttons

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,10 @@ import {
 	Route,
 	RouterProvider,
 } from "react-router-dom";
+import {
+	PayPalScriptProvider,
+	PaypalScriptProvider,
+} from "@paypal/react-paypal-js";
 import { Provider } from "react-redux";
 import store from "./store";
 // import "bootstrap/dist/css/bootstrap.min.css";
@@ -47,7 +51,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<RouterProvider router={router} />
+			<PayPalScriptProvider deferLoading={true}>
+				<RouterProvider router={router} />
+			</PayPalScriptProvider>
 		</Provider>
 	</React.StrictMode>
 );
