1cf012be6406f5fe77e1c3f81fa8ac06f8561e4e
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-01-05 13:02:36 -0600

    list users for admin

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -35,6 +35,13 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 				body: data,
 			}),
 		}),
+		getUsers: builder.query({
+			query: () => ({
+				url: USERS_URL,
+			}),
+			providesTags: ["Users"], //makes sure data is not cached so we don't have to re load to see update
+			keepUnusedDataFor: 5,
+		}),
 	}),
 });
 
@@ -43,4 +50,5 @@ export const {
 	useLogoutMutation,
 	useRegisterMutation,
 	useProfileMutation,
+	useGetUsersQuery,
 } = usersApiSlice;
