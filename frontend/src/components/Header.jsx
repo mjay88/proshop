24bc64d79a1690a857e1d80a8b7e309a9a33922c
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-01-05 22:25:26 -0600

    searchbox component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,6 +5,7 @@ import { LinkContainer } from "react-router-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../slices/authSlice";
 import logo from "../assets/logo.png";
+import SearchBox from "./SearchBox";
 import { useLogoutMutation } from "../slices/usersApiSlice";
 
 const Header = () => {
@@ -38,6 +39,7 @@ const Header = () => {
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ms-auto">
+							<SearchBox />
 							<LinkContainer to="/cart">
 								<Nav.Link>
 									<FaShoppingCart />
