1db1e87e7b7d256f6aa3602c4c26da3014fc1803
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-01-07 15:43:32 -0600

    preparing for deploy

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,6 @@ app.use(express.urlencoded({ extended: true })); //urlencoded data
 //Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-	res.send("API running beeyatch");
-});
 //use productRoutes in ./routes/productRoutes.js
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -38,6 +35,20 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve(); //set __dirname to current directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+//for production
+if (process.env.NODE_ENV === "production") {
+	//set static folder
+	app.use(express.static(path.join(__dirname, "/frontend/build")));
+	//any route that is not api will be redirected to index.html
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+	);
+} else {
+	app.get("/", (req, res) => {
+		res.send("API running beeyatch");
+	});
+}
 
 app.use(notFound);
 app.use(errorHandler);
